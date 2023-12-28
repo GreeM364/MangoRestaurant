0f667568b40f6e310836c217be82a58c184627b6
Denis <denisrubel40@gmail.com>
2023-12-28 02:17:41 +0200

    Implemented endpoints for CartController

---
+++ Mango.Services.ShoppingCart/Controllers/CartController.cs
@@ -0,0 +1,105 @@
+﻿using Mango.Services.ShoppingCartAPI.DTO;
+using Mango.Services.ShoppingCartAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : Controller
+    {
+        private readonly ICartRepository _cartRepository;
+        protected ResponseDto _response;
+
+        public CartController(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet("GetCart/{userId}")]
+        public async Task<object> GetCart(string userId)
+        {
+            try
+            {
+                CartDto cartDto = await _cartRepository.GetCartByUserId(userId);
+
+                _response.Result = cartDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("AddCart")]
+        public async Task<object> AddCart(CartDto cartDto)
+        {
+            try
+            {
+                CartDto cartDt = await _cartRepository.CreateUpdateCart(cartDto);
+
+                _response.Result = cartDt;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPut("UpdateCart")]
+        public async Task<object> UpdateCart(CartDto cartDto)
+        {
+            try
+            {
+                CartDto cartDt = await _cartRepository.CreateUpdateCart(cartDto);
+
+                _response.Result = cartDt;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpDelete("RemoveCart")]
+        public async Task<object> RemoveCart([FromBody]int cartId)
+        {
+            try
+            {
+                bool isSuccess = await _cartRepository.RemoveFromCart(cartId);
+
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpDelete("ClearCart/{userId}")]
+        public async Task<object> ClearCart(string userId)
+        {
+            try
+            {
+                bool isSuccess = await _cartRepository.ClearCart(userId);
+
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
